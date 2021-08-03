9a9d539869d2624162963165170e48c298845dc8
Zhivlova <NataZhivlova@yandex.ru>
2021-08-04 00:18:28 +0300

    Работа с заказом пользователя

--- new_shop/mainapp/admin.py
+++ new_shop/mainapp/admin.py
@@ -2,7 +2,16 @@ from django.contrib import admin
 from .models import ProductCategory, Product
 
 admin.site.register(ProductCategory)
-admin.site.register(Product)
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = [
+        'name',
+        'price',
+        'quantity',
+        'created',
+    ]
 
 
 
