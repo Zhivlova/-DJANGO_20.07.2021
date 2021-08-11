29599dd865306631c3062070d9d92636f57ddc2e
Zhivlova <NataZhivlova@yandex.ru>
2021-08-11 23:35:40 +0300

    Развертывание Django-проекта на VPS

--- new_shop/ordersapp/forms.py
+++ new_shop/ordersapp/forms.py
@@ -1,6 +1,8 @@
 from django import forms
 from ordersapp.models import Order, OrderItem
 
+from new_shop.mainapp.models import Product
+
 
 class OrderForm(forms.ModelForm):
     class Meta:
@@ -21,5 +23,6 @@ class OrderItemForm(forms.ModelForm):
 
     def __init__(self, *args, **kwargs):
         super(OrderItemForm, self).__init__(*args, **kwargs)
+        self.fields['product'].queryset = Product.get_items()
         for field_name, field in self.fields.items():
             field.widget.attrs['class'] = 'form-control'
--- new_shop/ordersapp/views.py
+++ new_shop/ordersapp/views.py
@@ -2,7 +2,7 @@ from django.db import transaction
 from django.db.models.signals import pre_save
 from django.dispatch import receiver
 from django.forms import inlineformset_factory
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, JsonResponse
 from django.shortcuts import get_object_or_404
 
 from django.urls import reverse_lazy, reverse
@@ -13,6 +13,8 @@ from basketapp.models import Basket
 from .forms import OrderForm, OrderItemForm
 
 from django.contrib.auth.mixins import LoginRequiredMixin
+
+from ..mainapp.models import Product
 
 
 class OrderList(LoginRequiredMixin, ListView):
@@ -148,3 +150,11 @@ def product_quantity_update_save(sender, update_fields, instance, **kwargs):
 def product_quantity_update_delete(sender, instance, **kwargs):
     instance.product.quantity += instance.quantity
     instance.product.save()
+
+    def get_product_price(request, pk):
+        if request.is_ajax():
+            product = Product.objects.filter(pk=int(pk)).first()
+            if product:
+                return JsonResponse({'price': product.price})
+            else:
+                return JsonResponse({'price': 0})
